70e9619fe582bc25a9709702fc61b5e91e85a550
Marcos de Matos Machado <www.marcosm.com@gmail.com>
2017-05-06 23:26:06 -0300

    Correções em scripts de ambiente

--- Front-end/docker-entrypoint.sh
+++ Front-end/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd learn-with-srs
 git pull 
+cd Front-end
 npm install 
 bower install --allow-root 
 http-server -p 8080
--- Service/docker-entrypoint.sh
+++ Service/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export SERVER_DB=172.18.0.1:25017
-echo "Pasta atual:" `pwd`
+cd learn-with-srs
 git pull 
+cd Service
 npm install
 bin/www 
