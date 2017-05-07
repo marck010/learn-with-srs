8796874332a4bce13af6f92cf3fbbeb5c1c4e0f6
Marcos de Matos Machado <www.marcosm.com@gmail.com>
2017-05-07 00:53:49 -0300

    Alteração no script de backup

--- backup-db.sh
+++ backup-db.sh
@@ -1,3 +1,3 @@
-
+#!/bin/bash
 sudo docker exec -t dictionary-database bash -c "cd /data/db && mongodump"
-sudo docker exec -it dictionary-database bash -c "sshpass -e scp /data/db/* marcos@172.18.0.1:`pwd`/Database/dump"
+sudo docker exec -it dictionary-database bash -c "sshpass -e scp -o StrictHostKeyChecking=no /data/db/* marcos@172.18.0.1:`pwd`/Database/dump"
